8720336771cfaac2020a613f6a73c2daf385c5c1
ksm0076 <60387895+ksm0076@users.noreply.github.com>
2022-11-20 20:07:46 +0900

    3 reply greeting function-ksm

--- greeting.js
+++ greeting.js
@@ -1,6 +1,15 @@
-const greeting = function (rtm, channel) {
+function randomNum(min, max) {
+  const randNum = Math.floor(Math.random() * (max - min + 1));
+  return randNum;
+}
+
+const reply = ['Hello!', 'Bongjur!', 'Nihao!'];
+
+function greeting(rtm, channel) {
   console.log('인사를 합시다.');
-  rtm.sendMessage('Hello!', channel);
-};
+  const n = randomNum(0, 2);
+  console.log(`n값 : ${n}`);
+  rtm.sendMessage(reply[n], channel);
+}
 
 module.exports = greeting;
