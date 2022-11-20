f500ece0436343cbaa619467d1582ac54dd6de46
Hyun <greg1028@naver.com>
2022-11-20 23:04:18 +0900

    dic_test

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ try {
 
 console.log(token);
 
+const dict = {
+  'Architectural Engineering': 'College of Engineering Building 1, 132',
+  'Mechanical Engineering': 'College of Engineering Building 4, 212',
+  'Urban Engineering': 'College of Engineering Building 9, 917',
+  'Electronic Engineering': 'College of Engineering Building 7, 224',
+  'Computer Science and Engineering': 'College of Engineering Building 7, 224',
+  'Chemical Engineering': 'College of Engineering Building 6, 999',
+  Accounting: 'College of Commerce 2, 9999',
+  'International Trade': 'College of Commerce 1, 9999',
+  'Korean Language and Literature': 'College of Humanities, 320',
+  'Library and Information Science': 'College of Humanities, 427',
+};
+
 token = token.replace(/\n/g, '');
 
 const rtm = new RTMClient(token);
@@ -26,6 +39,8 @@ rtm.on('message', (message) => {
 
   if (!isNaN(text)) {
     square(rtm, text, channel);
+  } else if (text in dict) {
+    console.log(dict[text]);
   } else if (isNaN(text)) {
     switch (text) {
       case 'hi':
