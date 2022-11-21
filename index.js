70445170eae3e778f9a36b2b4153bbb0c28d8b3e
aa <kmc_n@naver.com>
2022-11-21 19:10:40 +0900

    style(index): Change Style

--- index.js
+++ index.js
@@ -22,13 +22,12 @@ const square = require('./square');
 const schedule = require('./schedule');
 
 rtm.on('message', (message) => {
-        const { channel } = message;
-  	const { text } = message;
+  const { channel } = message;
+  const { text } = message;
 
-        if(text=='학사일정'){
-                rtm.sendMessage('안내 받을 날짜를 이야기해주세요', channel);
-        }else if(isNaN(text)){
-                square(rtm, text, channel);
-        }else
-                rtm.sendMessage('ERROR2', channel);
+  if (text == '학사일정') {
+    rtm.sendMessage('안내 받을 날짜를 이야기해주세요', channel);
+  } else if (isNaN(text)) {
+    square(rtm, text, channel);
+  } else { rtm.sendMessage('ERROR2', channel); }
 });
