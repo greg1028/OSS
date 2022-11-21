ba01cc5b50852c6caf4f421193d01c6b6d254d8d
Hyun <greg1028@naver.com>
2022-11-21 21:32:10 +0900

    Feat#4 modulization with mocha

--- office.spec.js
+++ office.spec.js
@@ -1,9 +1,36 @@
 const office = require('./office');
 
+const offarr = [
+  'College of Engineering Building 1, 132',
+  'College of Engineering Building 4, 212',
+  'College of Engineering Building 9, 917',
+  'College of Engineering Building 7, 224',
+  'College of Engineering Building 7, 224',
+  'College of Engineering Building 6, 999',
+  'College of Commerce 2, 9999',
+  'College of Commerce 1, 9999',
+  'College of Humanities, 320',
+  'College of Humanities, 427',
+];
+
 describe('App test!', () => {
   it('addresses 검사', (done) => {
-    text = office('Architectural Engineering');
-    if (text === 'College of Engineering Building 1, 132') {
+    text1 = office.office('Architectural Engineering');
+    if (text1 in offarr) {
+      done();
+    } else {
+      throw new Error('Test Failed');
+    }
+
+    text2 = office.office('Korean Language and Literature');
+    if (text2 in offarr) {
+      done();
+    } else {
+      throw new Error('Test Failed');
+    }
+
+    text3 = office.office('Library and Information Science');
+    if (text3 in offarr) {
       done();
     } else {
       throw new Error('Test Failed');
