5fd269567960264b3684c18cf12b76d6dad94a17
Hyun <greg1028@naver.com>
2022-11-21 21:44:40 +0900

    Feat#4 with mocha office.spec.js

--- office.spec.js
+++ office.spec.js
@@ -1,39 +1,22 @@
 const office = require('./office');
 
-const offarr = [
-  'College of Engineering Building 1, 132',
-  'College of Engineering Building 4, 212',
-  'College of Engineering Building 9, 917',
-  'College of Engineering Building 7, 224',
-  'College of Engineering Building 7, 224',
-  'College of Engineering Building 6, 999',
-  'College of Commerce 2, 9999',
-  'College of Commerce 1, 9999',
-  'College of Humanities, 320',
-  'College of Humanities, 427',
-];
-
 describe('App test!', () => {
   it('addresses 검사', (done) => {
     text1 = 'Architectural Engineering';
-    if (office.office(text1) in offarr) {
-      done();
-    } else {
+    console.log(office.office(text1));
+    if (office.office(text1) !== 'College of Engineering Building 1, 132') {
       throw new Error('Test Failed');
     }
 
     text2 = 'Korean Language and Literature';
-    if (office.office(text2) in offarr) {
-      done();
-    } else {
+    if (office.office(text2) !== 'College of Humanities, 320') {
       throw new Error('Test Failed');
     }
 
     text3 = 'Library and Information Science';
-    if (office.office(text3) in offarr) {
-      done();
-    } else {
+    if (office.office(text3) !== 'College of Humanities, 427') {
       throw new Error('Test Failed');
     }
+    done();
   });
 });
