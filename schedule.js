435634542bcb21f02bb88f253b8a0d834b543b62
aa <kmc_n@naver.com>
2022-11-21 19:12:46 +0900

    style(schedule): Change Style

--- schedule.js
+++ schedule.js
@@ -1,14 +1,12 @@
-const schedule = function (rtm, text, channel){
-
-        const fs = require('fs')
-        const array = fs.readFileSync('haksa.txt').toString().split("\n")
-        for(i in array){
-                if(array[i].includes(text)){
-                        rtm.sendMessage(array[i], channel);
-                        break;
-                }else{
-                }
-        }
-}
-
-module.exports = schdule;
+const schedule = function (rtm, text, channel) {
+  const fs = require('fs');
+  const array = fs.readFileSync('haksa.txt').toString().split('\n');
+  for (i in array) {
+    if (array[i].includes(text)) {
+      rtm.sendMessage(array[i], channel);
+      break;
+    } else {
+    }
+  }
+};
+module.exports = schedule;
