1c6bf902423eab7834b73f46061b2fb8f950f185
aa <kmc_n@naver.com>
2022-11-23 18:26:10 +0900

    코드개선

--- schedule.js
+++ schedule.js
@@ -1,9 +1,9 @@
 const schedule = function (rtm, text, channel) {
   const fs = require('fs');
-  const array = fs.readFileSync('haksa.js').toString().split('\n');
-  for (i in array) {
-    if (array[i].includes(text)) {
-      rtm.sendMessage(array[i], channel);
+  const date = fs.readFileSync('haksa.js').toString().split('\n');
+  for (i in date) {
+    if (date[i].includes(text)) {
+      rtm.sendMessage(date[i], channel);
       break;
     } else {
     }
