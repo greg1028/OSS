7812770f75158364c1bdcb1f9a2e8148801b8054
aa <kmc_n@naver.com>
2022-11-23 15:27:22 +0900

    chore(index): 자잘한 코드 개선

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ token = token.replace(/\n/g, '');
 const rtm = new RTMClient(token);
 rtm.start();
 
+const greeting = require('./greeting');
+const square = require('./square');
 const schedule = require('./schedule');
 const scheduleinfo = require('./haksa');
 
@@ -28,7 +30,15 @@ rtm.on('message', (message) => {
     rtm.sendMessage('안내 받을 날짜를 이야기해주세요', channel);
   } else if (text in scheduleinfo) {
     schedule(rtm, text, channel);
-  } else if (text in Info) {
-    rtm.sendMessage(getAdress(text), channel);
+  } else if (!isNaN(text)) {
+    square(rtm, text, channel);
+  } else {
+    switch (text) {
+      case 'hi':
+        greeting(rtm, channel);
+        break;
+      default:
+        rtm.sendMessage('I am alive~', channel);
+    }
   }
 });
