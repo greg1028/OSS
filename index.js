f383abcbce9d057a847304d9c42e2a5a4849a69a
aa <kmc_n@naver.com>
2022-11-23 13:53:22 +0900

    chore(index): index code improvement

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ token = token.replace(/\n/g, '');
 const rtm = new RTMClient(token);
 rtm.start();
 
-const greeting = require('./greeting');
-const square = require('./square');
 const schedule = require('./schedule');
+const scheduleinfo = require('./haksa');
 
 rtm.on('message', (message) => {
   const { channel } = message;
@@ -27,7 +26,13 @@ rtm.on('message', (message) => {
 
   if (text == '학사일정') {
     rtm.sendMessage('안내 받을 날짜를 이야기해주세요', channel);
-  } else if (isNaN(text)) {
-    square(rtm, text, channel);
-  } else { rtm.sendMessage('ERROR2', channel); }
+  } else if (text in scheduleinfo) {
+    schedule(rtm, text, channel);
+  } else {
+    rtm.sendMessage('ERROR', channel);
+  }
+
+  if (text in Info) {
+    rtm.sendMessage(getAdress(text), channel);
+  }
 });
