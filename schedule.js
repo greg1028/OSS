819cc2a7336441c79eef201d7629403999fefa40
ksm0076 <60387895+ksm0076@users.noreply.github.com>
2022-11-23 21:44:47 +0900

    (style) eslint rule

--- schedule.js
+++ schedule.js
@@ -1,12 +1,13 @@
-const schedule = function (rtm, text, channel) {
-  const fs = require('fs');
+/* eslint-disable no-restricted-syntax */
+const fs = require('fs');
+
+function schedule(rtm, text, channel) {
   const date = fs.readFileSync('haksa.js').toString().split('\n');
   for (i in date) {
     if (date[i].includes(text)) {
       rtm.sendMessage(date[i], channel);
       break;
-    } else {
     }
   }
-};
+}
 module.exports = schedule;
