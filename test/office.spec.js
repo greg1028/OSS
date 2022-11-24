32875dd64e9b62205daa78ae3b415b4e81463b4e
ksm0076 <60387895+ksm0076@users.noreply.github.com>
2022-11-24 17:16:43 +0900

    (test) improve 'office.spec.js'

--- test/office.spec.js
+++ test/office.spec.js
@@ -4,18 +4,17 @@ text1 = 'Architectural Engineering';
 text2 = 'Korean Language and Literature';
 text3 = 'Library and Information Science';
 
-describe('App test!', () => {
-  it('addresses 검사', (done) => {
-    if (getAdress(text1) !== 'College of Engineering Building 1, 132') {
-      throw new Error(`Test Failed, Can't reply '${text}'`);
-    }
-    if (getAdress(text2) !== 'College of Humanities, 320') {
-      throw new Error(`Test Failed, Can't reply '${text}'`);
-    }
-
-    if (getAdress(text3) !== 'College of Humanities, 427') {
-      throw new Error(`Test Failed, Can't reply '${text}'`);
-    }
+describe('office test!', () => {
+  it("Test - Put 'Architectural Engineering'", (done) => {
+    assert.equal(getAdress(text1), 'College of Engineering Building 1, 132');
+    done();
+  });
+  it("Test - Put 'Korean Language and Literature'", (done) => {
+    assert.equal(getAdress(text2), 'College of Humanities, 320');
+    done();
+  });
+  it("Test - Put 'Architectural Engineering'", (done) => {
+    assert.equal(getAdress(text3), 'College of Humanities, 427');
     done();
   });
 });
