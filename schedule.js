81fa391554854500904d51f1238b8fa6fdd839ea
aa <kmc_n@naver.com>
2022-11-24 21:26:03 +0900

    chore(schedule): 코드개선

--- schedule.js
+++ schedule.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-restricted-syntax */
 const fs = require('fs');
 
-function schedule(rtm, text, channel) {
+function schedule(text) {
   const date = fs.readFileSync('haksa.js').toString().split('\n');
   for (i in date) {
     if (date[i].includes(text)) {
-      rtm.sendMessage(date[i], channel);
-      break;
+      return date[i];
     }
   }
 }
