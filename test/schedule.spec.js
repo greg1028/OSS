b3a953757b27923a0ce5d84538cf61d83fbc7b8f
Hyun <greg1028@naver.com>
2022-12-08 18:11:17 +0900

    (chore) fix unicode

--- test/schedule.spec.js
+++ test/schedule.spec.js
@@ -7,15 +7,15 @@ const text3 = '12/21';
 
 describe('schedule test!', () => {
   it("Test - Put '10/15'", (done) => {
-    assert.equal(schedule(text1), '10/15는 개교기념일입니다.');
+    assert.equal(schedule(text1), '10/15개교기념일.');
     done();
   });
   it("Test - Put '8/22'", (done) => {
-    assert.equal(schedule(text2), '8/22는 후기 학위수여식입니다.');
+    assert.equal(schedule(text2), '8/22후기 학위수여식.');
     done();
   });
   it("Test - Put '12/21'", (done) => {
-    assert.equal(schedule(text3), '12/21는 종강입니다.');
+    assert.equal(schedule(text3), '12/21종강.');
     done();
   });
 });
