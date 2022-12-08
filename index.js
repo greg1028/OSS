561609977b68fc3ca790ced3065497367f54bc34
ksm0076 <60387895+ksm0076@users.noreply.github.com>
2022-12-08 18:01:52 +0900

    (style) 이번주메뉴 case문에 넣음

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const menu = require('./menu');
 const rating = require('./rating');
 const parse = require('./parse');
 
-let sch = false; // 학사 일정 상태
-let offi = false; // 학과 사무실 상태
+let sch = false; // 학사일정 상태
+let offi = false; // 학과사무실 상태
 
 rtm.on('message', (message) => {
   const { channel } = message;
@@ -37,14 +37,14 @@ rtm.on('message', (message) => {
   console.log(message);
   console.log(text);
 
-  if (sch) {
+  if (sch) { // 학사일정 입력 받았을 때
     if (text in scheduleinfo) {
       rtm.sendMessage(schedule(text), channel);
     } else {
       rtm.sendMessage('유효하지 않은 일정 입력입니다.', channel);
     }
     sch = false;
-  } else if (offi) {
+  } else if (offi) { // 학과안내 입력 받았을 때
     if (text in Info) {
       rtm.sendMessage(getAdress(text), channel);
     } else {
@@ -53,8 +53,6 @@ rtm.on('message', (message) => {
     offi = false;
   } else if (!isNaN(text)) { // 제곱 기능
     square(rtm, text, channel);
-  } else if (text === '이번주뭐나와') { // 이번주 메뉴 평점
-    rating(rtm, channel);
   } else if (isNaN(text)) {
     switch (text) {
       case 'hi':
@@ -68,8 +66,11 @@ rtm.on('message', (message) => {
         rtm.sendMessage('안내받을학과사무실을이야기해주세요', channel);
         offi = true;
         break;
-      case '밥':
+      case '밥': // 오늘 메뉴
         menu(rtm, channel);
+        break;
+      case '이번주뭐나와': // 이번주 메뉴 평점
+        rating(rtm, channel);
         break;
       default:
         rtm.sendMessage('명령어\n 1. 학사일정\n 2. 학과안내\n 3. 밥\n 4.이번주뭐나와', channel);
