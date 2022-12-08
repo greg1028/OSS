c7727110bc4d6567cc9943ff31e99cba96c84010
Hyun <greg1028@naver.com>
2022-12-08 21:52:38 +0900

    (chore) use haksa.txt

--- test/schedule.spec.js
+++ test/schedule.spec.js
@@ -7,15 +7,15 @@ const text3 = '12/21';
 
 describe('schedule test!', () => {
   it("Test - Put '10/15'", (done) => {
-    assert.equal(schedule(text1), '개교기념일 \r');
+    assert.equal(schedule(text1), '개교기념일\r');
     done();
   });
   it("Test - Put '8/22'", (done) => {
-    assert.equal(schedule(text2), '후기 학위수여식 \r');
+    assert.equal(schedule(text2), '후기 학위수여식\r');
     done();
   });
   it("Test - Put '12/21'", (done) => {
-    assert.equal(schedule(text3), '종강 \r');
+    assert.equal(schedule(text3), '종강\r');
     done();
   });
 });
