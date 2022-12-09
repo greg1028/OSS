7cd0123d3d543bdf56a22ab855df3b7c1a1bbcd4
Hyun <greg1028@naver.com>
2022-12-09 14:16:42 +0900

    (chore) fix schedule.spec

--- test/schedule.spec.js
+++ test/schedule.spec.js
@@ -7,15 +7,15 @@ const text3 = '12/21';
 
 describe('schedule test!', () => {
   it("Test - Put '10/15'", (done) => {
-    assert.equal(schedule(text1), '개교기념일\r');
+    assert.equal(schedule(text1).replace(/\r/g, ''), '개교기념일');
     done();
   });
   it("Test - Put '8/22'", (done) => {
-    assert.equal(schedule(text2), '후기 학위수여식\r');
+    assert.equal(schedule(text2).replace(/\r/g, ''), '후기 학위수여식');
     done();
   });
   it("Test - Put '12/21'", (done) => {
-    assert.equal(schedule(text3), '종강\r');
+    assert.equal(schedule(text3).replace(/\r/g, ''), '종강');
     done();
   });
 });
