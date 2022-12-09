423cedfc124478bea85f6b1c1cda90ebb4ec41ef
Hyun <greg1028@naver.com>
2022-12-09 21:36:47 +0900

    (feat) 거리기반 추천시스템

--- dept.js
+++ dept.js
@@ -1,16 +1,33 @@
+const dis = require('editdistance');
 const fs = require('fs');
 
 dept = fs.readFileSync('./dept.txt').toString('utf-8');
 const parse = require('./parse');
 
+answer = 'basic';
+let rcom = 'basic';
+distance = 999;
 function Adress(text) {
   for (const i of dept.split('\n')) {
     if (parse(i.split(' - ')[0]) === parse(text)) {
       console.log(i.split(' - ')[1]);
       return i.split(' - ')[1];
     }
-    // else() 글자 거리 기반
+
+    if (editdistance.eval(parse(text), parse(i.split(' - ')[0])) < distance) {
+      rcom = text;
+      distnace = dis.eval(parse(text), parse(i.split(' - ')[0]));
+    }
   }
+
+  for (const i of dept.split('\n')) {
+    if (parse(i.split(' - ')[0]) === parse(rcom)) {
+      console.log(i.split(' - ')[1]);
+
+      return `${rcom}를 말씀하시는 건가요? ${i.split(' - ')[1]} 입니다.`;
+    }
+  }
+  return '잘못된 입력입니다.';
 }
 
 module.exports = Adress;
