a1782cc31754c9355a465b6888bf3457fbbc4caa
Hyun <greg1028@naver.com>
2022-12-09 14:50:42 +0900

    (chore) use dpet.txt

--- test/office.spec.js
+++ test/office.spec.js
@@ -7,15 +7,15 @@ const text3 = 'Library and Information Science';
 
 describe('office test!', () => {
   it("Test - Put 'Architectural Engineering'", (done) => {
-    assert.equal(getAdress(text1), 'College of Engineering Building 1, 132');
+    assert.equal(getAdress(text1).replace(/\r/g, ''), 'College of Engineering Building 1, 132');
     done();
   });
   it("Test - Put 'Korean Language and Literature'", (done) => {
-    assert.equal(getAdress(text2), 'College of Humanities, 320');
+    assert.equal(getAdress(text2).replace(/\r/g, ''), 'College of Humanities, 320');
     done();
   });
   it("Test - Put 'Architectural Engineering'", (done) => {
-    assert.equal(getAdress(text3), 'College of Humanities, 427');
+    assert.equal(getAdress(text3).replace(/\r/g, ''), 'College of Humanities, 427');
     done();
   });
 });
